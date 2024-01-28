444fbf0a21ecc225683b8cabe4b5ad608906f27a
marcelohcortez <marcelohcortez@gmail.com>
2024-01-28 11:10:09 +0100

    Fix an error on theme setting

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,7 +12,10 @@ const theme = createTheme({
             h1: { fontSize: rem(36) },
         },
     },
-    primaryColor: 'deepBlue',
+    primaryColor: 'bright-pink',
+    colors: {
+        'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
+    },
     primaryShade: 6,
     autoContrast: true,
     defaultRadius: 'md',
