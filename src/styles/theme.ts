73cf0b96308e15654d0339ca045877e2a8989320
marcelohcortez <marcelohcortez@gmail.com>
2024-02-10 12:24:54 +0100

    Swap theme from Mantine to Material UI

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,25 +1,15 @@
-import { createTheme, rem } from '@mantine/core';
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
 
-const theme = createTheme({  
-    shadows: {
-        md: '1px 1px 3px rgba(0, 0, 0, .25)',
-        xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
     },
-    
-    headings: {
-        fontFamily: 'Roboto, sans-serif',
-        sizes: {
-            h1: { fontSize: rem(36) },
-        },
+    secondary: {
+      main: green[500],
     },
-    primaryColor: 'bright-pink',
-    colors: {
-        'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
-    },
-    primaryShade: 6,
-    autoContrast: true,
-    defaultRadius: 'md',
-    cursorType: 'pointer',
+  },
 });
 
-export { theme }
+export { theme };
