82388ae69468a7ed244ccda44e67d33b70de7907
marcelohcortez <marcelohcortez@gmail.com>
2024-02-15 19:25:18 +0100

    Create the view for the 'Budget - Single'

--- src/views/Budgets/Single/index.tsx
+++ src/views/Budgets/Single/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { getBudget } from 'api/budgets';
+import { useParams } from 'react-router-dom';
+import { Budget as BudgetType } from 'types/budget';
+import { getBudgetStatus } from 'api/budgetsStatus';
+import { fetchBudgetById, fetchBudgetStatusById } from './fetches';
 
 const BudgetsSingle = () => {
+  const { id } = useParams<{ id: string }>();
+
+  if (!id) {
+    return <div>No Budget Id found, check you URL</div>;
+  }
+
+  const [budget, setBudget] = useState<BudgetType | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchBudgetById(id, setBudget).then(() =>
+      fetchBudgetStatusById(id, setBudget, budget),
+    );
+  }, [id]);
+
+  useEffect(() => {
+    if (budget && budget.status !== '' && budget.status !== undefined) {
+      setIsLoading(false);
+    }
+  }, [budget]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>BUDGETS ID</div>
-  )
-}
+    <>
+      {budget && (
+        <>
+          <div>BUDGET {budget._id}</div>
+          <div key={budget._id}>
+            <div>Total: {budget.total}</div>
+            <div>Project: {budget.project}</div>
+            <div>Client: {budget.client}</div>
+            <div>Status: {budget.status}</div>
+            <div>Created By: {budget.created_by}</div>
+            <div>Edited By: {budget.edited_by}</div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
 export default BudgetsSingle;
