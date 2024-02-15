4cb718b5e8f55a4679a41c83f7f1756fccbb4cb2
marcelohcortez <marcelohcortez@gmail.com>
2024-02-15 19:23:18 +0100

    Fix budget type

--- src/types/budget.d.ts
+++ src/types/budget.d.ts
@@ -1,15 +1,14 @@
-import { Project } from "./project";
-import { Client } from "./client";
-import { BudgetStatus } from "./budgetStatus";
-import { User } from "./user";
+import { Project } from './project';
+import { Client } from './client';
+import { User } from './user';
 interface Budget {
-    _id: string,
-    total: number,
-    project: Project._id,
-    client: Client._id,
-    status: BudgetStatus.status,
-    created_by: User._id,
-    edited_by?: User._id,
-}   
+  _id: string;
+  total: string;
+  project: Project._id;
+  client: Client._id;
+  status: string;
+  created_by: User._id;
+  edited_by?: User._id;
+}
 
 export type { Budget };
