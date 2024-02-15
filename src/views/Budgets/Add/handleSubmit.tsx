1f9b772d3282a8380993db376497d48521001c8e
marcelohcortez <marcelohcortez@gmail.com>
2024-02-15 19:27:23 +0100

    Refactor teh creationg of a new button and a new status when a form is submited

--- src/views/Budgets/Add/handleSubmit.tsx
+++ src/views/Budgets/Add/handleSubmit.tsx
@@ -3,15 +3,24 @@ import { FormikHelpers } from 'formik';
 
 import { budgetFormValues } from 'types/budgetFormValues';
 import { createBudget } from 'api/budgets';
+import { createBudgetStatus } from 'api/budgetsStatus';
 
 const handleSubmit = async (
   values: budgetFormValues,
   { resetForm }: FormikHelpers<budgetFormValues>,
   setSuccess: (value: boolean) => void,
   setError: (value: boolean) => void,
-): Promise<void> => {
+) => {
   try {
-    await createBudget(values);
+    // create a new budget
+    const budgetResponse = await createBudget(values);
+
+    // if the budget was created, create a new status for taht budget
+    if (budgetResponse._id) {
+      await createBudgetStatus(values.status.status, budgetResponse._id);
+      console.log('VALUES: ', values.status.status, budgetResponse._id);
+    }
+
     setSuccess(true);
     setTimeout(() => {
       setSuccess(false);
