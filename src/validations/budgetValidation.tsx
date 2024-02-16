df5e69736e9763fb25eef08a78bd7870ce601d13
marcelohcortez <marcelohcortez@gmail.com>
2024-02-16 17:02:01 +0100

    Add regex to budgetValidationSchema

--- src/validations/budgetValidation.tsx
+++ src/validations/budgetValidation.tsx
@@ -1,13 +1,13 @@
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-  total: Yup.string().required('Total is required'),
+  total: Yup.string()
+    .required('Total is required')
+    .matches(/^\d+$/, "The 'Total' field should contain only numbers"),
   project: Yup.object().shape({
-    _id: Yup.string().required('Project ID is required'),
     name: Yup.string().required('Project name is required'),
   }),
   client: Yup.object().shape({
-    _id: Yup.string().required('Client ID is required'),
     name: Yup.string().required('Client name is required'),
   }),
   status: Yup.object().shape({
